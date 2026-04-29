fcd1c5b6f506803c9aec9b7a23712939d7cff313
Silvana Yulia Damara <silvanayuliadamara@gmail.com>
2026-04-29 20:59:46 +0700

    chore: apply clean code improvements

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,19 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use App\Interfaces\MovieRepositoryInterface;
+use App\Repositories\MovieRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        $this->app->bind(MovieRepositoryInterface::class, MovieRepository::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        Paginator::useBootstrap();
+        //
     }
 }
